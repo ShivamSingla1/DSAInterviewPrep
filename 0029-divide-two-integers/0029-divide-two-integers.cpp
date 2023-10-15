829028d4b5df9301ac2231d40de2080efbdfa685
Shivam Singla <76487947+ShivamSingla1@users.noreply.github.com>
2023-10-15 18:20:05 +0530

    Time: 0 ms (100%), Space: 6.2 MB (54.1%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -4,6 +4,21 @@ public:
         if(dividend==INT_MIN && divisor==-1) return INT_MAX;
         if(dividend==INT_MIN && divisor==1) return INT_MIN;
 
-        return dividend/divisor;
+        long int dd = abs(dividend), dv = abs(divisor);
+        
+        int res=0;
+        while(dv<=dd) {
+            long int mul=dv, tmp=1;
+            while(mul<=dd-mul) {
+                mul+=mul;
+                tmp+=tmp;
+            }
+            res+=tmp;
+            dd-=mul;
+        }
+        
+        if((dividend<0&&divisor>0) || (dividend>0&&divisor<0)) return -res;
+        
+        return res;
     }
 };
